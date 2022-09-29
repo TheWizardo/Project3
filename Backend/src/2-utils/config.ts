cf111efed64e4d6160c25906cf56d0cb7c341779
TheWizardo <oz.sabbag@gmail.com>
2022-09-29 04:16:33 +0300

    development

--- Backend/src/2-utils/config.ts
+++ Backend/src/2-utils/config.ts
@@ -6,17 +6,6 @@ class Config {
     public mysqlHost = "localhost";
     public mysqlUser = "root";
     public mysqlPassword = "";
-    
-    // public mysqlDatabase = "project3";
-    // public mysqlHost = "project3.c7awgtjmgpxz.us-east-1.rds.amazonaws.com";
-    // public mysqlUser = "admin";
-    // public mysqlPassword = "IThinkYouAreWrongToWantAHeart";
-
-    // public port = process.env.PORT;
-    // public mysqlHost = process.env.MYSQL_HOST;
-    // public mysqlUser = process.env.MYSQL_USER;
-    // public mysqlPassword = process.env.MYSQL_PASSWORD;
-    // public mysqlDatabase = process.env.MYSQL_DATABASE;
     public imagesFolder = path.resolve(__dirname, "..", "1-assets", "images");
 }
 
--- Frontend/src/Utils/config.ts
+++ Frontend/src/Utils/config.ts
@@ -1,6 +1,6 @@
 class Config {
     public vacationCount = 10;
-    public baseURL = "http://localhost:3002/api";
+    public baseURL = "http://localhost:3002/api";//"https://proj3-backend-production.up.railway.app/api";
     public authURL = `${this.baseURL}/auth`;
     public vacationsURL = `${this.baseURL}/vacations`;
     public imagesURL = `${this.baseURL}/images`;
